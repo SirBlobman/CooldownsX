8fe6b20e18c9cb23708664df60e4d13dfd57f12b
SirBlobman <blobmansrealm@gmail.com>
2024-12-20 18:56:02 -0500

    Update to Java 17 and GradleUp Shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,8 +44,8 @@ subprojects {
     version = rootProject.version
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     repositories {
